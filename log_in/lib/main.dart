bc252073f94b5185aa94b6d0d5aadda2e4de7fee
PradhumnJadhav <pradhumnjadav@gmail.com>
2024-04-10 16:38:46 +0530

    pj commiting .....

--- log_in/lib/main.dart
+++ log_in/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:log_in/home.dart';
 import 'package:log_in/l10n/l10n.dart';
@@ -29,6 +29,7 @@ void main() async {
       GlobalMaterialLocalizations.delegate,
       GlobalCupertinoLocalizations.delegate,
       GlobalWidgetsLocalizations.delegate,
+
     ],
   ));
 }
