53654d461158abbeb3601064a62ff866af4e3e55
PradhumnJadhav <pradhumnjadav@gmail.com>
2024-04-10 16:56:18 +0530

    adding ocpp

--- log_in/lib/main.dart
+++ log_in/lib/main.dart
@@ -4,14 +4,15 @@ import 'package:log_in/home.dart';
 import 'package:log_in/l10n/l10n.dart';
 import 'package:log_in/login.dart';
 import 'package:log_in/register.dart';
-import 'package:log_in/checkuser.dart';
+import 'package:log_in/resetPassword.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:log_in/splash_screen.dart';
 import 'package:log_in/mapPage.dart';
-import 'package:log_in/resetPassword.dart';
-
+import 'package:log_in/chargePoint.dart';
+ 
+import 'package:log_in/chargePointHome.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -25,7 +26,10 @@ void main() async {
       'splash_screen': (context) => Mysplash(),
       'flutter': (context) => HomePage(title: 'flutter'),
       'map': (context)=> SimpleMap(),
-      'resetPassword':(context) => ResetPasswordPage(),
+      'resetPassword':(context)=> ResetPasswordPage(),
+      'chargePoint' :(context) => ChargePoint(),
+      'chargePointHome' :(context) => ChargePointHome(),
+      
     },
     supportedLocales: L10n.all,
     localizationsDelegates: [
--- log_in/lib/mapPage.dart
+++ log_in/lib/mapPage.dart
@@ -1,13 +1,39 @@
 import 'dart:async';
-
+ 
+ 
+ 
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:log_in/login.dart';
+import 'package:web_socket_client/web_socket_client.dart';
+import 'package:firebase_database/firebase_database.dart';
+ 
+
+final firebaseApp = Firebase.app();
+final rtdb = FirebaseDatabase.instanceFor(app: firebaseApp , databaseURL: 'https://my-project-1579067571295-default-rtdb.firebaseio.com/');
+
+final socket = WebSocket(Uri.parse('ws://172.20.25.116:9000/test1'),timeout: Duration(seconds: 30));
+
+
+LatLng tempPos = LatLng(22.7196,75.8577);
+
+
+
+
+ 
+
+ 
+ 
+
+
 
 class SimpleMap extends StatefulWidget {
   @override
@@ -15,6 +41,10 @@ class SimpleMap extends StatefulWidget {
 }
 
 class _SimpleMapState extends State<SimpleMap> {
+ 
+
+ 
+
   final Completer<GoogleMapController> _controller = Completer();
 
   static const CameraPosition _kInitialPosition = CameraPosition(
@@ -22,6 +52,25 @@ class _SimpleMapState extends State<SimpleMap> {
       zoom: 4,
       tilt: 0,
       bearing: 0);
+
+final TextEditingController _serverController = TextEditingController();
+ 
+  
+
+     
+  
+pushData(int data,DatabaseReference ref,WebSocket socket){
+
+  socket.send('[2, "12345", "Authorize", { "idTag":"pradhumn"   }]');
+   socket.messages.listen((message) {
+   print(message);
+});
+  Navigator.pushNamed(context, 'chargePointHome');
+
+
+ 
+  }
+
 
   signOut() async {
     await FirebaseAuth.instance.signOut();
@@ -31,6 +80,26 @@ class _SimpleMapState extends State<SimpleMap> {
       ),
     );
   }
+  mapUpdate()async{
+  String uid='pj1';
+final ref = FirebaseDatabase.instance.ref();
+ final lat= await ref.child('chargePoint/${uid}/lattitde').get();
+  final lng= await ref.child('chargePoint/${uid}/longitude').get();
+ double ln=double.parse(lng.value.toString());
+ double lt=double.parse(lat.value.toString());
+ print(lt+ln);
+  myMarker.add(Marker(markerId: MarkerId("next"),
+   
+
+  position:LatLng(ln,lt),
+     infoWindow: const InfoWindow(
+            title: 'Cu   Location',
+            
+            
+          )
+  ),
+  );
+}
 
   final List<Marker> myMarker = [];
   final List<Marker> markerList = [
@@ -41,6 +110,7 @@ class _SimpleMapState extends State<SimpleMap> {
     super.initState();
     myMarker.addAll(markerList);
     packdata();
+    // mapUpdate();
   }
 
   Future<Position> getUserLocation() async {
@@ -52,12 +122,14 @@ class _SimpleMapState extends State<SimpleMap> {
 
     return await Geolocator.getCurrentPosition();
   }
-
+      
+ 
+//  
   packdata() {
     getUserLocation().then((value) async {
       // print('My Location');
       // print('${value.latitude} ${value.longitude}');
-
+  
       myMarker.add(Marker(
           markerId: MarkerId('Second'),
           position: LatLng(value.latitude, value.longitude),
@@ -65,9 +137,11 @@ class _SimpleMapState extends State<SimpleMap> {
           infoWindow: const InfoWindow(
             title: 'Current Location',
             
+            
           )));
+          
       CameraPosition cameraPosition = CameraPosition(
-        target: LatLng(value.latitude, value.longitude),
+        target: LatLng(value.latitude, value.longitude+2),
         zoom: 15,
       );
 
@@ -79,6 +153,7 @@ class _SimpleMapState extends State<SimpleMap> {
     });
   }
 
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -95,12 +170,30 @@ class _SimpleMapState extends State<SimpleMap> {
             },
           ),
 
-          Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+                 Container(
+                  child: TextButton(
+            onPressed: () {
+                 DatabaseReference ref = FirebaseDatabase.instance.ref("users/123");
+                  pushData(12, ref,socket);
 
+               
+            },
+            child: Text(
+               'Stations Nearby',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                  decoration: TextDecoration.underline,
+                  color: Color.fromARGB(255, 236, 44, 10),
+                  fontSize: 18),
+            ),
+            style: ButtonStyle(),
+          ),
+                 ),
               Container(
-                padding:EdgeInsets.only(top: 625),
+                // padding:EdgeInsets.only(top: 625),
                 child: TextButton(
                 onPressed: () {
                   signOut();
@@ -117,8 +210,34 @@ class _SimpleMapState extends State<SimpleMap> {
                 style: ButtonStyle(),
               ),
               ),
+               Container(
+                // padding:EdgeInsets.only(top: 625),
+                child: TextButton(
+                onPressed: () {
+                   mapUpdate();
+                 
+                },
+                child: Text(
+                  
+                  'Refresh',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      color: Colors.red,
+                      fontSize: 23),
+                ),
+                style: ButtonStyle(),
+              ),
+              ),
+               
+               
+           
+             
+            
+
+
               Container(
-                padding:EdgeInsets.only(right: 05,top: 475),
+                // padding:EdgeInsets.only(right: 05,top: 475),
                 child: FloatingActionButton(
                   
                   onPressed: () {
